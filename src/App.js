ebe1ec6db6e4c5afb0a9de6b68d63614e857d2a7
JakubGitC <jakubciolek@proton.me>
2023-06-05 22:43:39 +0200

    adding new files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Article from "./components/article/Article";
+import Brand from "./components/brand/Brand";
+import CTA from "./components/cta/CTA";
+import Feature from "./components/feature/Feature";
+import Navbar from "./components/navbar/Navbar";
+import {
+  Blog,
+  Features,
+  Footer,
+  Header,
+  Possibility,
+  What,
+} from "./containers";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <What />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 }
---
+++ src/containers/what/What.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./what.css";
+const What = () => {
+  return <div>What</div>;
+};
+
+export default What;
